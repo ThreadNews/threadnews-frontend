e32c545a0d77c29684fa731de0f21a71b3d40ef0
Jack-kelly-22 <Jacklaxjk@gmail.com>
2021-02-15 17:34:51 -0800

    basic ui

--- threadnews/src/App.js
+++ threadnews/src/App.js
@@ -1,9 +1,11 @@
+
 import CategoryBubbleSet from './category-bubbles'
 import './css/App.css';
 import  axios from 'axios';
 import Top_nav from './Nav';
 import './css/bootstrap_theme.css';
 import React from "react";
+import TopicBubble from './TopicBubble'
 import {useState, useEffect,useRef} from 'react'
 import Sidebar from './Sidebar';
 import {Container, Row, Col,} from 'react-bootstrap';
@@ -16,75 +18,59 @@ const test_data = [{"name":"policy","backgroundColor":"#EE1B2D","textColor": "wh
 
 
 
-
-
-
-//export default function bubbleMenu()
-  async function fetch_category_bubble_data(){
-   try{
-     let d;
-     const r = await axios.get("http://localhost:5000/categoryBubbleData")
-      .then(response => {
-        console.log(response.data);
-        console.log(response.status);
-        console.log(response.statusText);
-        console.log(response.headers);
-        console.log(response.config);
-        d = response.data
-        return response.data.topic_bubble_data
-      });
-      
-      return r;
-     
-   }
-   catch(error){
-     console.log(error)
-     return false
-   }
- }
-
-
-
 class App extends React.Component {
+
   constructor(){
     super();
-    this.state = {interests:[],num:0}
+    this.state = {interests:[],num:0,topics:[],bub:[]}
     this.add_interest = this.add_interest.bind(this);
     this.remove_interest = this.remove_interest.bind(this);
+   
   }
+
   
   add_interest(new_interest){
     let interests = this.state.interests;
     let n =[...interests,new_interest];
     // this.state.interests = n;
-    console.log(this.state.interests);
-    this.setState({interests:n, num: this.state.num + 1});
+    //console.log(this.state.interests);
+    this.setState({interests:n, num: this.state.num + 1,});
     console.log('num:', this.state.num);
+    
   }
+
 
   remove_interest(old_interest){
     let n = this.state.interests;
     n = n.filter(interest => !(interest===old_interest));
     console.log('Removed:', old_interest)
     console.log("remaining", n)
-    this.setState({interests:n, num: this.state.num - 1});
+    this.setState({interests:n, num: this.state.num - 1,topics:this.state.topics});
   }
 
 
   //let bubble_data = fetch_category_bubble_data()
   //console.log(bubble_data)
   render() {  
+    
     return (
       <div className="App" width='100%'>
-        {Top_nav()}
+        <Top_nav/>
         <Container >
           <Row>
             <Col sm ={9}> 
-              { CategoryBubbleSet({
+              {/* { CategoryBubbleSet({
                 // interests:interests,
                 add_interest: this.add_interest,
-                data:{"topic_bubble_data": [{"logo_path": "weather.svg", "bg_color": "#68C56A"}, {"logo_path": "Soccer.svg", "bg_color": "#68B1C5"}, {"logo_path": "photography.svg", "bg_color": "#C568B1"}, {"logo_path": "android.svg", "bg_color": "#68B1C5"}, {"logo_path": "music.svg", "bg_color": "#A268C5"}, {"logo_path": "drawing.svg", "bg_color": "#C3C568"}, {"logo_path": ".DS_Store", "bg_color": "#8BC568"}, {"logo_path": "Podcasts.svg", "bg_color": "#B468C5"}, {"logo_path": "travel.svg", "bg_color": "#C3C568"}, {"logo_path": "stocks.svg", "bg_color": "#C5688B"}, {"logo_path": "Esports.svg", "bg_color": "#B468C5"}, {"logo_path": "TV.svg", "bg_color": "#C5A268"}, {"logo_path": "crypto.svg", "bg_color": "#68C3C5"}, {"logo_path": "Cars.svg", "bg_color": "#C5688B"}, {"logo_path": "movies.svg", "bg_color": "#68C3C5"}, {"logo_path": "finance.svg", "bg_color": "#6879C5"}, {"logo_path": "Surfing.svg", "bg_color": "#B1C568"}, {"logo_path": "ocean.svg", "bg_color": "#68B1C5"}, {"logo_path": "Housing.svg", "bg_color": "#C568C3"}, {"logo_path": "Football.svg", "bg_color": "#C56879"}, {"logo_path": "voting.svg", "bg_color": "#7C68C5"}, {"logo_path": "cloud.svg", "bg_color": "#7C68C5"}, {"logo_path": "Videogames.svg", "bg_color": "#C56879"}, {"logo_path": "math.svg", "bg_color": "#8F68C5"}, {"logo_path": "diy.svg", "bg_color": "#688BC5"}, {"logo_path": "Psychology.svg", "bg_color": "#C3C568"}, {"logo_path": "Hockey.svg", "bg_color": "#8F68C5"}, {"logo_path": "Wearables.svg", "bg_color": "#C5A268"}, {"logo_path": "computers.svg", "bg_color": "#C3C568"}, {"logo_path": "privacy.svg", "bg_color": "#C568C3"}]}})}
-                
+                interests:this.interests
+              })} */}
+                <CategoryBubbleSet
+                  add_interest={this.add_interest}
+                  interests={this.state.interests}
+                  topics = {this.state.topics}
+                  
+                  
+                />
             </Col>
             
             <Col sm={2}>
@@ -92,9 +78,10 @@ class App extends React.Component {
                 num = {this.state.num}
                 interests = {this.state.interests}
                 remove_interest = {this.remove_interest}
+                onClick={this.props.onFinish}
                 >
                   
-                </Sidebar>
+                </Sidebar >
             </Col>
           </Row>
         </Container>
--- threadnews/src/Sidebar.js
+++ threadnews/src/Sidebar.js
@@ -1,50 +1,49 @@
 import { extend } from "jquery";
 import App from './App';
 import React from 'react';
+import {useState, useEffect,useRef} from 'react'
 import ProgressBar from 'react-customizable-progressbar';
 import './css/bootstrap_theme.css';
 import './css/sidebar.css';
 import Card from 'react-bootstrap/Card';
+import {primary_color, secondary_color} from './Colors'
 import Button from "react-bootstrap/Button";
-
+import axios from 'axios';
 import ListGroup from 'react-bootstrap/ListGroup'
-
-
-// class Sidebar extends React.Component {
-
-
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         topics : [],
-    //         channels : [],
-    //         num : props.num
-    //     };
-    // }
     
 
 export function Sidebar(props){
     console.log("so update", props.num)
+
+    function update_user_interests(){
+        axios.post('http://127.0.0.1:5000/update_interests',{user_id:"test user id", new_interests:props.interests})
+        console.log("updated user interests")
+        props.onClick()
+    }
+
     return (
         <div>
         <Card
             bg="Warning" 
-            style = {{width:'20rem',paddingTop:'30px'}}
+            style = {{width:'20rem',paddingTop:'30px',borderRadius:'30px'}}
         >
             <Card.Body>
                 
                 <Card.Title className="sidebarHeader">Progress</Card.Title>
-                <Card.Subtitle>please select {props.num-5} more</Card.Subtitle>
+                <Card.Subtitle>please select {5 - props.num} more</Card.Subtitle>
                 <ProgressBar 
                     progress={props.num/5 * 100}
                     radius={100}
-                    strokeColor="#93E38B"
+                    strokeColor={primary_color}
                 >
                     <div className="indicator">
                         {props.num}/8
                     </div>
                 </ProgressBar>
-                <Button variant={props.num<5 ? "outline-success": "Success" } size ="lg" >
+                <Button variant={props.num<5 ? "outline-warning": "Success" } 
+                    disabled={props.num<5?true:false}
+                    onClick={update_user_interests}
+                    size ="lg" >
                     Continue
                 </Button>
                 <ListGroup variant="flush">
