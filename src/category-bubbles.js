fa2243934294bd9a42264b7e18c363382cef6ba4
Jack-kelly-22 <60161371+Jack-kelly-22@users.noreply.github.com>
2021-05-11 20:48:52 -0700

    fix formating

--- src/category-bubbles.js
+++ src/category-bubbles.js
@@ -1,42 +1,41 @@
-import BubbleUI from 'react-bubble-ui'
-//import bubbleMenu from './App';
-import {useState, useEffect,useRef} from 'react'
-import 'react-bubble-ui/dist/index.css';
-import './css/bubbleSet.css';
-import  axios from 'axios';
-import {primary_color, secondary_color} from './Colors'
-import { topic_data } from './topic_data.js'
-import TopicBubble from './TopicBubble'
-// #import Child from './ChildComponent';
-//const axios = require('axios')
-export default function CategoryBubbleSet(props){
+import BubbleUI from "react-bubble-ui";
 
-    const options = {
-		size: 180,
-		minSize: 100,
-		gutter: 30,
-		provideProps: false,
-		numCols: 4,
-		fringeWidth: 120,
-		yRadius: 150,
-		xRadius: 600,
-		cornerRadius: 50,
-		// showGuides: true,
-		compact: true,
-		gravitation: 5
-	}	
-	// let data = props.data;
-	// let b = Array.from(data.topic_bubble_data);
-	// // var interests = [];
-	// console.log(b);
-	console.log(topic_data);
-	const children = topic_data.map((data, i) => {
-		console.log("dd")
-        // return <TopicBubble {...data} key={i} className='child' onClick={()=>add_interest(data.logo_path.slice(0,-4))} />
-		return <TopicBubble {...data} key={i} className='child'  onClick={()=>props.add_interest(data.topic)}/>
-    });
+import "react-bubble-ui/dist/index.css";
+import "./css/bubbleSet.css";
 
-   return (<BubbleUI options={options} className="bubbleSet">
-       {children}
-   </BubbleUI>)
+import { topic_data } from "./topic_data.js";
+import TopicBubble from "./TopicBubble";
+
+export default function CategoryBubbleSet(props) {
+  const options = {
+    size: 180,
+    minSize: 100,
+    gutter: 30,
+    provideProps: false,
+    numCols: 4,
+    fringeWidth: 120,
+    yRadius: 150,
+    xRadius: 600,
+    cornerRadius: 50,
+    // showGuides: true,
+    compact: true,
+    gravitation: 5,
+  };
+
+  const children = topic_data.map((data, i) => {
+    return (
+      <TopicBubble
+        {...data}
+        key={i}
+        className="child"
+        onClick={() => props.add_interest(data.topic)}
+      />
+    );
+  });
+
+  return (
+    <BubbleUI options={options} className="bubbleSet">
+      {children}
+    </BubbleUI>
+  );
 }
