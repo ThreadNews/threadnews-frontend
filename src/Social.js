6e49f5bacbf96d8abdbf50c089fb8410804ce1c5
Jack-kelly-22 <60161371+Jack-kelly-22@users.noreply.github.com>
2021-05-11 20:50:00 -0700

    functions in Social.js

--- src/Social.js
+++ src/Social.js
@@ -1,5 +1,5 @@
-
 import axios from "axios";
+
 export async function follow(user_id){
     let token = sessionStorage.getItem('access_token');
     let data = {user_id:user_id,action:'follow'};
@@ -7,3 +7,33 @@ export async function follow(user_id){
     console.log("User follow attempt")
     axios.post("http://127.0.0.1:5000/follow_user",data,head);
 }
+
+
+export async function repost_article(articleId){
+    let token = sessionStorage.getItem('access_token')
+    let data = {action:'add',article_id:articleId}
+    let head = {headers:{Authorization:"Bearer "+ token}}
+    axios.post('http://127.0.0.1:5000/repost',data,head)
+  }
+
+
+export function like_article(articleId){
+    let token = sessionStorage.getItem('access_token')
+    let data = {action:'add',article_id:articleId}
+    console.log("data:", data)
+    let head = {headers:{Authorization:"Bearer "+ token}}
+    axios.post('http://127.0.0.1:5000/like',data,head)
+    
+  }
+
+export function save_article(articleId,saved){
+    let data = { action: "add", article_id: articleId };
+    let token = sessionStorage.getItem("access_token");
+    let head = { headers: { Authorization: "Bearer " + token } };
+    if (saved) {
+        data.action = "delete";
+    }
+    axios.post("http://127.0.0.1:5000/save", data, head);
+}
+
+
