fd9dfdd8f964cfdd224d52eaf841e925c2849f53
Jack-kelly-22 <60161371+Jack-kelly-22@users.noreply.github.com>
2021-05-13 18:22:03 -0700

    class to function

--- src/InterestPage.js
+++ src/InterestPage.js
@@ -2,71 +2,60 @@
 import CategoryBubbleSet from './category-bubbles'
 import './css/App.css';
 import './css/bootstrap_theme.css';
-import React from "react";
+import React, {useState } from "react";
 import Sidebar from './Sidebar';
 import {Container, Row, Col,} from 'react-bootstrap';
 
+export default function InterestPage(props){
 
-class InterestPage extends React.Component {
+  const [interests,setInterests] = useState([]);
 
-  constructor(){
-    super();
-    this.state = {interests:[],num:0,topics:[],bub:[]}
-    this.add_interest = this.add_interest.bind(this);
-    this.remove_interest = this.remove_interest.bind(this);
-   
-  }
-
-  
-  add_interest(new_interest){
-    let interests = this.state.interests;
+  function add_interest(new_interest){
     let n =[...interests,new_interest];
-    this.setState({interests:n, num: this.state.num + 1,});
-    console.log('num:', this.state.num);
+    setInterests(n);
   }
 
-  remove_interest(old_interest){
-    let n = this.state.interests;
+  function remove_interest(old_interest){
+    let n = interests;
     n = n.filter(interest => !(interest===old_interest));
     console.log('Removed:', old_interest)
-    console.log("remaining", n)
-    this.setState({interests:n, num: this.state.num - 1,topics:this.state.topics});
+    console.log("remaining", n);
+    setInterests(n);
   }
 
-  render() {  
-    return (
-      <div className="App" width='100%'>
-        <div style = {{paddingTop:'40px',borderRadius:'30px',alignItems:'center'}}>
-          <h2>Welcome to Thread-News</h2>
-          <hr style={{width:'40%'}}></hr>
-          <h4>Please select some topics to get started</h4>
-        </div>
-        <Container >
-          <Row >
-            <Col xs={{span:7,offset:2}} className=""> 
-              <div className="bubbles">
-                <CategoryBubbleSet
-                  add_interest={this.add_interest}
-                  interests={this.state.interests}
-                  topics = {this.state.topics}
-                  className="bubbles"
-                />
-                </div>
-            </Col>
-            
-            <Col xs={"2"}>
-              <Sidebar 
-                num = {this.state.num}
-                interests = {this.state.interests}
-                remove_interest = {this.remove_interest}
-                >
-                </Sidebar >
-            </Col>
-          </Row>
-        </Container>
+  return (
+    <div className="App" width='100%'>
+      <div style = {{paddingTop:'40px',borderRadius:'30px',alignItems:'center'}}>
+        <h2>Welcome to Thread-News</h2>
+        <hr style={{width:'40%'}}></hr>
+        <h4>Please select some topics to get started</h4>
       </div>
-    );
-  }
+      <Container >
+        <Row >
+          <Col xs={{span:7,offset:2}} className=""> 
+            <div className="bubbles">
+              <CategoryBubbleSet
+                add_interest={add_interest}
+                interests={interests}
+                topics = {props.topics}
+                className="bubbles"
+              />
+              </div>
+          </Col>
+          
+          <Col xs={"2"}>
+            <Sidebar 
+              num = {interests.length}
+              interests = {interests}
+              remove_interest = {remove_interest}
+              >
+              </Sidebar >
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
+   
 
-export default InterestPage;
+
