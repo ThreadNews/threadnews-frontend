a38adaea16a11e51c005d97265843880cf1a5338
Jack-kelly-22 <60161371+Jack-kelly-22@users.noreply.github.com>
2021-05-23 16:34:58 -0700

    align button and search bar on same line

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,32 +1,29 @@
-import { Button, Row, Col } from "react-bootstrap";
+//react imports
 import React, { useState, useCallback } from "react";
+import { Button } from "react-bootstrap";
 import axios from "axios";
-import {UserBlockList} from './UserBlock'
-// import "./css/bootstrap_theme.css";
+//css imports
 import "../css/search.css";
-require('dotenv').config()
+// requre env variables
+require("dotenv").config();
+
 export function SearchBar(props) {
   const [search_string, setSearchstring] = useState("");
-  const [user_ids, set_user_ids] = useState([]); 
-
+  const [user_ids, set_user_ids] = useState([]);
 
   function search_user(search_string) {
     let token = sessionStorage.getItem("access_token");
     let head = { headers: { Authorization: "Bearer " + token } };
-    let data = {username: search_string};
+    let data = { username: search_string };
     //not currently implemented on backend going to fix
     axios
-      .post(
-        process.env.REACT_APP_BACKEND_URL + "/search_user",
-        data,
-        head
-      )
+      .post(process.env.REACT_APP_BACKEND_URL + "/search_user", data, head)
       .then((result) => {
         if (result) {
           // console.log("finished searching user", result);
           if (result.status === 200) {
             console.log("t1", result.data.users);
-            set_user_ids(result.data.users)
+            set_user_ids(result.data.users);
             console.log("USER ids", user_ids);
           } else {
             console.log("ERROR");
@@ -48,20 +45,15 @@ export function SearchBar(props) {
           placeholder="Username.."
           onChange={(v) => setSearchstring(v.target.value)}
         ></input>
-      </form>
 
-      <Button
-        variant="secondary"
-        className="submit"
-        onClick={() => search_user(search_string)}
-      >
-        {" "}
-        search
-      </Button>
-      <div>
-        {/* <UserBlockList user_ids = {user_ids}/> */}
-        {user_ids}
-      </div>
+        <Button
+          variant="secondary"
+          className="submit"
+          onClick={() => search_user(search_string)}
+        >
+          search
+        </Button>
+      </form>
     </div>
   );
 }
